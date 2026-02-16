bbe0b7d912619d7190a3e993d435cae2963cf819
Einar Arvidsson <tooten21@gmail.com>
2026-02-16 23:36:48 +0100

    Added button grid

--- game.py
+++ game.py
@@ -1,6 +1,24 @@
 from tkinter import *
 from tkinter import ttk
 
+# Set up main application window
 root = Tk()
 root.title("Tic-Tac-Toe")
+
+# Create a content frame
+mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+
+# Insert the frame into the user interface
+mainframe.grid(column=0, row=0)
+
+# Create button grid
+for i in range(3):
+    for j in range(3):
+        ttk.Button(mainframe).grid(row=i, column=j)
+
+# Add polish
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=3, pady=3)
+
+
 root.mainloop()
