c8dd4bcd9d087315a70026e8e898d4f764710815
Einar Arvidsson <tooten21@gmail.com>
2026-02-16 23:48:30 +0100

    Added button grid

--- game.py
+++ game.py
@@ -14,11 +14,11 @@ mainframe.grid(column=0, row=0)
 # Create button grid
 for i in range(3):
     for j in range(3):
-        ttk.Button(mainframe).grid(row=i, column=j)
+        ttk.Button(mainframe, width=5).grid(row=i, column=j)
 
 # Add polish
 for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=3, pady=3)
+    child.grid_configure(padx=10, pady=10)
 
 
 root.mainloop()
