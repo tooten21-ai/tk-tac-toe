e86f41e3dd6177c39a702f2343d77f6e7e1ff733
Einar Arvidsson <tooten21@gmail.com>
2026-02-17 11:46:56 +0100

    Added button grid to a 2D list

--- game.py
+++ game.py
@@ -12,9 +12,16 @@ mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
 mainframe.grid(column=0, row=0)
 
 # Create button grid
-for i in range(3):
-    for j in range(3):
-        ttk.Button(mainframe, width=5).grid(row=i, column=j)
+button_grid = [
+    [],
+    [],
+    []
+]
+for r in range(3):
+    for c in range(3):
+        button = ttk.Button(mainframe, width=5)
+        button.grid(row=r, column=c)
+        button_grid[r].append(button)
 
 # Add polish
 for child in mainframe.winfo_children(): 
